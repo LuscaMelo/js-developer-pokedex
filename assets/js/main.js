8bb1a1922e1056e60ceb8c897a14a52a303c741d
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-04-25 01:45:25 -0300

    Desafio finalizado, adicionado cards individuais para cada pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const modal = document.getElementById('modal')
+const body = document.querySelector('body')
 
 const maxRecords = 151
 const limit = 10
@@ -7,15 +9,16 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-
-            <div class="detail">
-                <ol class="types">
+        <li class="pokemon ${pokemon.type}" onClick="showDetails(${pokemon.number})">
+            <div class="details">
+                <span class="number">${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <ul class="types">
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
+                </ul>
+            </div>
 
+            <div class="img">
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
@@ -45,3 +48,47 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+function showDetails(id) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    modal.classList.remove('hide')
+    overlay.classList.remove('hide')
+    body.classList.add('overflow-hidden')
+
+    pokeApi.getAllPokemonDetails(url)
+        .then((pokemon) => modalBody.innerHTML +=
+            `
+            <li class="modal-pokemon">
+                <div class="${pokemon.types[0].type.name}">
+                    <img src="${pokemon.sprites.other.dream_world.front_default}" class="${pokemon.types[0].type.name}" alt="${pokemon.name}">
+                </div>
+                <div class="modal-heading ${pokemon.types[0].type.name}">
+                    <span class="number">${pokemon.id}</span>
+                    <span class="name">${pokemon.name}</span>
+                </div>
+                <div class="modal-body">
+                    <div class="size">
+                            <p>Height: <span>${pokemon.height}</span></p>
+                            <p>Weight: <span>${pokemon.weight}</span></p>
+                    </div>
+                    <p class="detail">Types:</p>
+                    <div class="types">
+                            ${pokemon.types.map(item => `<span class="${item.type.name}">${item.type.name}</span>`).join('')}</span>
+                    </div>
+                    <p class="detail">Abilities:</p>
+                    <div class="abilities">
+                            ${pokemon.abilities.map(item => `<div>${item.ability.name}</div>`).join('')}
+                    </div>
+                </div>
+            </li>
+            `)
+}
+
+
+function closeModal() {
+    modal.classList.add('hide')
+    overlay.classList.add('hide')
+    body.classList.remove('overflow-hidden')
+    modalBody.innerHTML = ''
+}
